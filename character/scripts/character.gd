f04f07999d41af3b7ce39b3c9981f2b710bb67ad
Breno <brenolfm@unipam.edu.br>
2024-05-02 15:48:28 -0300

    Animation

--- character/scripts/character.gd
+++ character/scripts/character.gd
@@ -19,7 +19,7 @@ func _move() -> void:
 	
 	if _direction != Vector2.ZERO:
 		velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _acceleration)
-		velocity.x = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)
+		velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)
 		return
 		
 	velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _friction)
