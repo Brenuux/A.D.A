2729a24d7958b4b7737efd66dae12ebf83641828
Chrystian <chrystiansousa@unipam.edu.br>
2024-05-17 15:12:32 -0300

    colisao casa

--- menu/menu.gd
+++ menu/menu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _on_iniciar_pressed():
-	get_tree().change_scene_to_file("res://levels/scenes/test_level_2.tscn")
+	get_tree().change_scene_to_file("res://levels/scenes/inicio.tscn")
 
 
 
