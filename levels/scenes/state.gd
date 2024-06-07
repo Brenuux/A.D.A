50323ae827c2965f5c9ffbc7266035809ce0fdcc
Chrystian <chrystiansousa@unipam.edu.br>
2024-06-07 10:38:40 -0300

    boss development

--- levels/scenes/state.gd
+++ levels/scenes/state.gd
@@ -1,17 +1,25 @@
 extends Node2D
 class_name State
 
+@onready var debug = owner.find_child("debug")
+@onready var player = owner.get_parent().find_child("player")
+@onready var animation_player = owner.find_child("AnimationPlayer")
+
 func _ready():
 	set_physics_process(false)
-	
-func _enter():
+
+func enter():
 	set_physics_process(true)
-	
+
 func exit():
 	set_physics_process(false)
-	
-func _transition():
+
+func transition():
 	pass
-	
+
 func _physics_process(_delta):
 	transition()
+	if debug != null:
+		debug.text = name
+	else:
+		print("Nó debug não encontrado!")
