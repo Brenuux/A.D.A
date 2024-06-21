6a79e1a5a36acd7bc16a6649675946bbfe9b6949
Chrystian <chrystiansousa@unipam.edu.br>
2024-06-21 10:19:39 -0300

    corrigindo 1

--- character/scripts/character.gd
+++ character/scripts/character.gd
@@ -1,14 +1,30 @@
 extends CharacterBody2D
+class_name Player
+
+var _state_machine
+var is_dead: bool = false
+var _is_attacking: bool = false
 
 @export_category("Variables")
 @export var _move_speed: float = 64.0
-
 @export var _friction: float = 0.2
 @export var _acceleration: float = 0.4
 
+@export_category("Objects")
+@export var _attack_timer: Timer = null
+@export var _animation_tree: AnimationTree = null
+
+func _ready() -> void:
+	_animation_tree.active = true
+	_state_machine = _animation_tree["parameters/playback"]
 
 func _physics_process(_delta: float) -> void:
+	if is_dead:
+		return
+		
 	_move()
+	_attack()
+	_animate()
 	move_and_slide()
 
 func _move() -> void:
@@ -16,8 +32,11 @@ func _move() -> void:
 		Input.get_axis("move_left", "move_right"),
 		Input.get_axis("move_up", "move_down")
 	)
-	
 	if _direction != Vector2.ZERO:
+		_animation_tree["parameters/idle/blend_position"] = _direction
+		_animation_tree["parameters/walk/blend_position"] = _direction
+		_animation_tree["parameters/attack/blend_position"] = _direction
+		
 		velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _acceleration)
 		velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)
 		return
@@ -25,7 +44,34 @@ func _move() -> void:
 	velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _friction)
 	velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _friction)
 
+func _attack() -> void:
+	if Input.is_action_just_pressed("attack") and _is_attacking == false:
+		set_physics_process(false)
+		_attack_timer.start()
+		_is_attacking = true
+	
+func _animate() -> void:
+	if _is_attacking:
+		_state_machine.travel("attack")
+		$"swing-espada".play()
+		return
+		
+	if velocity.length() > 2:
+		_state_machine.travel("walk")
+		return
+		
+	_state_machine.travel("idle")
 
-	velocity = _direction.normalized() * _move_speed
-	
-	
+func _on_attack_timer_timeout() -> void:
+	set_physics_process(true)
+	_is_attacking = false
+
+func _on_attack_area_body_entered(_body) -> void:
+	if _body.is_in_group("enemy"):
+		_body.apply_damage(20)  # Chamando a função apply_damage com o dano desejado
+
+func die() -> void:
+	is_dead = true
+	_state_machine.travel("death")
+	await get_tree().create_timer(1.0).timeout
+	get_tree().reload_current_scene()
--- character/scripts/slime.gd
+++ character/scripts/slime.gd
@@ -3,64 +3,62 @@ class_name Slime
 
 var _is_dead: bool = false
 var _player_ref = null
-
+var health: int = 100  # Vida inicial do inimigo
 
 @export_category("Objects")
 @export var _texture: Sprite2D = null
 @export var _animation: AnimationPlayer = null
 
-
-#codigo verificando quem entrou na area de colisão é do grupo "character" para para seguir
 func _on_detection_area_body_entered(_body) -> void:
 	if _body.is_in_group("character"):
 		_player_ref = _body
-	
 
-
-# caso o personagem do grupo "character" sair da colisão, slime não vai seguir
 func _on_detection_area_body_exited(_body):
 	if _body.is_in_group("character"):
 		_player_ref = null
-	
+
 func _physics_process(_delta: float) -> void:
 	if _is_dead:
 		return
-		
+
 	_animate()
 	if _player_ref != null:
 		if _player_ref.is_dead:
 			velocity = Vector2.ZERO
 			move_and_slide()
 			return
-			
-			
-		#vai retornar a direção do slime e personagem
+
 		var _direction: Vector2 = global_position.direction_to(_player_ref.global_position)
 		var _distance: float = global_position.distance_to(_player_ref.global_position)
-			
-		if _distance < 20:
-			_player_ref.die()
 
-			
 		velocity = _direction * 30
 		move_and_slide()
 
 func _animate() -> void:
 	if velocity.x > 0:
 		_texture.flip_h = false
-	
+
 	if velocity.x < 0:
 		_texture.flip_h = true
-	
+
 	if velocity != Vector2.ZERO:
 		_animation.play("walk")
 		return
-		
+
 	_animation.play("idle")
+
+func apply_damage(amount: int) -> void:
+	if _is_dead:
+		return
+		
+	health -= amount
+	if health <= 0:
+		update_health()
 
 func update_health() -> void:
 	_is_dead = true
 	_animation.play("death")
 
 func _on_animation_finished(_anim_name: String) -> void:
-	queue_free()
+	if _anim_name == "death":
+		queue_free()
